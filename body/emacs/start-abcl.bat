95ec79d207f1c7c0914d9b9f2887e3436cf43088
nakrakiiya <n.akr.akiiya@gmail.com>
2013-02-25 10:44:04 +0800

    Add required files.

---
+++ body/emacs/start-abcl.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM Set Path for lib32
+call %~dp0\..\lib32\setpath.bat
+
+cd /d %~dp0
+set HOME=%~dp0\home
+
+SET CURRENT_COMMON_LISP_TYPE=ABCL
+start bin\runemacs.exe --debug-init
---
+++ body/emacs/start-ccl64.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM Set Path for lib64
+call %~dp0\..\lib64\setpath.bat
+
+cd /d %~dp0
+set HOME=%~dp0\home
+
+SET CURRENT_COMMON_LISP_TYPE=CCL64
+start bin\runemacs.exe --debug-init
