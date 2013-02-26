5851bf9eb80b46ff413fa259660e0e6ef4912a25
nakrakiiya <n.akr.akiiya@gmail.com>
2013-02-26 18:24:08 +0800

    FIX: wrong path for hyperspec .

--- body/clisp/startup.lisp
+++ body/clisp/startup.lisp
@@ -1,4 +1,4 @@
 ;; set the PATH of the hyperSpec
-(setf *CLHS-ROOT-DEFAULT* (format nil "~a" (truename "../emacs/home/.emacs.d/clhs7/")))
+(setf *CLHS-ROOT-DEFAULT* (format nil "~a" (truename "../emacs/home/.emacs.d/clhs7/HyperSpec/")))
 
 
