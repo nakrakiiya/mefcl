2570b425082fa7cda1da361babff926772e6295a
nakrakiiya <n.akr.akiiya@gmail.com>
2013-03-18 11:30:55 +0800

    Now `grep directory', `grep directory recursively' works.

--- body/lib32/setpath.bat
+++ body/lib32/setpath.bat
@@ -5,3 +5,6 @@ SET TMP=%~dp0..\emacs\tmp
 
 SET PATH=%~dp0;%PATH%
 SET PATH=%~dp0\gtk\bin;%~dp0..\tcl\bin;"%~dp0..\jvm\bin";%PATH%
+
+REM SET PATH FOR MSYS/MINGW
+SET PATH=%~dp0\..\mingw\bin;%~dp0\..\mingw\msys\1.0\bin;%PATH%
--- body/lib64/setpath.bat
+++ body/lib64/setpath.bat
@@ -7,3 +7,6 @@ SET PATH=%~dp0;%PATH%
 
 REM though it's a 32-bits Tcl/Tk
 SET PATH=%~dp0..\tcl\bin;%PATH%
+
+REM SET PATH FOR MSYS/MINGW though it's 32-bits 
+SET PATH=%~dp0\..\mingw\bin;%~dp0\..\mingw\msys\1.0\bin;%PATH%
